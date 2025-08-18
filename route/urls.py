f5efe6d57e6d8e50ee89471287c94e80ae5e3ed8
unknown <ash1823@naver.com>
2025-08-19 00:29:15 +0900

    fix: url

--- route/urls.py
+++ route/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("create", RouteCollectionView.as_view(), name="route_create"),
-    path("/result", RouteByQuestionnaireView.as_view(), name="question_result_route")
+    path("result", RouteByQuestionnaireView.as_view(), name="question_result_route")
 ]
