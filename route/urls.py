1d7d61c739666ae3fc2f702f96813f8b0c1e2931
unknown <ash1823@naver.com>
2025-08-19 22:05:24 +0900

    feat: url 추가

--- route/urls.py
+++ route/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path("create", RouteCollectionView.as_view(), name="route_create"),
     path("result", RouteByQuestionnaireView.as_view(), name="question_result_route"),
-    path("select", AllPlacesSimpleView.as_view(), name="select_places_in_list")
+    path("select", AllPlacesSimpleView.as_view(), name="select_places_in_list"),
+    path('save', TravelPlanCreateView.as_view(), name='save-place'),
 ]
