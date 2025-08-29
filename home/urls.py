988d5050dfcd7b74a3684fc743b975bfbdf680fc
unknown <ash1823@naver.com>
2025-08-30 03:22:16 +0900

    fix: 즐겨찾기 추가 삭제 url

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', PlaceItemAllView.as_view(), name='place-all'),
     path('place/create', PlaceItemCreateView.as_view(), name='place-create'),
     path('place', PlaceItemDetailView.as_view(), name='place-detail'),
+    path('place/favorite', TogglePlaceFavoriteView.as_view(), name='place-favorite'),
 ]
