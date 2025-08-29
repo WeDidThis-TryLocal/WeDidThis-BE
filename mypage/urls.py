c182bcbfad105e80356338443f6d2dfa4b2850b0
unknown <ash1823@naver.com>
2025-08-30 03:12:33 +0900

    feat: 즐겨찾기 리스트

--- mypage/urls.py
+++ mypage/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path("history", MyTripListView.as_view(), name="my_trip_history"),
+    path("favorites", MyFavoriteListView.as_view(), name="my_favorite_places"),
 ]
--- mypage/views.py
+++ mypage/views.py
@@ -5,7 +5,11 @@ from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from route.models import *
+from home.models import *
 from home.permissions import IsTouristUser
+from home.views import get_first_image
+
+TYPE_LABEL_MAP = dict(PlaceItem.TYPE_CHOICES)
 
 
 @permission_classes([IsAuthenticated, IsTouristUser])
@@ -25,4 +29,25 @@ class MyTripListView(APIView):
         
         return Response({
             "results": items
+        }, status=status.HTTP_200_OK)
+    
+
+@permission_classes([IsAuthenticated, IsTouristUser])
+class MyFavoriteListView(APIView):
+    def get(self, request):
+        favs = (PlaceFavorite.objects.select_related("place").filter(user=request.user).order_by("place__name"))
+
+        results = []
+        for f in favs:
+            p = f.place
+            results.append({
+                "name": p.name,
+                "type": p.type,
+                "type_label": TYPE_LABEL_MAP.get(p.type),
+                "description": p.description,
+                "image": get_first_image(p.name)
+            })
+
+        return Response({
+            "results": results
         }, status=status.HTTP_200_OK)
