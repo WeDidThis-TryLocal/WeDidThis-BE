210d9052145da04a9ef9053479671d36b271e28d
unknown <ash1823@naver.com>
2025-08-31 11:33:51 +0900

    feat: admin.py 반영

--- mypage/admin.py
+++ mypage/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import AccountDeletionLog
 
-# Register your models here.
+
+class AccountDeletionLogAdmin(admin.ModelAdmin):
+    list_display = ("id", "user_id_snapshot", "user_name_snapshot", "account_id_snapshot", "reason", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user_name_snapshot", "account_id_snapshot", "reason")
+    ordering = ("-created_at",)
+
+
+admin.site.register(AccountDeletionLog, AccountDeletionLogAdmin)
