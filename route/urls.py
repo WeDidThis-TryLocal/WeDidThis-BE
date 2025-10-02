017fd532363e3bb84ff00f8858467bb7e35c1609
unknown <ash1823@naver.com>
2025-10-02 19:56:02 +0900

    fix: urls.py

--- route/urls.py
+++ route/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     # 직접 경로 설정 - 위치 저장
     path("save", TravelPlanCreateView.as_view(), name='save-place'),
     # 직접 경로 설정 - 테스트용 경로 생성
-    # path("build", SubmissionBuildRouteView.as_view(), name="build_route_by_gpt"),
+    path("build", SubmissionBuildRouteLocalView.as_view(), name="build_route_by_gpt"),
     # 직접 경로 생성 - GPT 경로 생성
-    path("build", SubmissionBuildRoutebyGPTView.as_view(), name="build_route_by_gpt"),
+    # path("build", SubmissionBuildRoutebyGPTView.as_view(), name="build_route_by_gpt"),
     # 경로 조회
     path("result/detail", RouteResultbySubmissionView.as_view(), name="route_result_by_submission"),
     # 경로 삭제
